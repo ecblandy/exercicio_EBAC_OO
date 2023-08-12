667a7c0dec024cd338880eb7a55e3ccac59a8065
Vinícius Blandy <viny19381@hotmail.com>
2023-08-12 18:03:07 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,26 @@
+function Pessoa (nome, idade){
+    this.nome = nome
+    this.idade = idade
+    this.saudacao = function() {
+        console.log(`${this.nome} olá, tudo bem?`)
+    }
+}
+
+function Funcionario (nome, idade, salario, cargo) {
+    this.salario = salario
+    this.cargo = cargo
+
+    Pessoa.call(this, nome, idade)
+}
+
+function TimeDeFutebol (time, nome, idade) {
+    this.time = time
+    Pessoa.call(this, nome, idade)
+}
+
+const joao = new Pessoa('João', 30)
+pessoa1.saudacao()
+const vinicius = new Funcionario('Vinicius', 25, 5000, 'Dev front end')
+console.log(vinicius)
+const mike = new TimeDeFutebol('Bahia', 'Mike', 28)
+console.log(TimeDeFutebol)
